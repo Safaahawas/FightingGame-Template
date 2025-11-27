df7b11895bec59970b086dce266cd20be4f8a1f2
CS_safaa20 <155908113+Safaahawas@users.noreply.github.com>
2025-11-27 19:45:10 +0300

    Add sprite class and player/enemy instances

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0,0,canvas.width,canvas.height)
+
+class sprite{
+    constructor(position){
+      this.position = position)
+  }
+
+  draw(){
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, 50, 150)
+  }
+}
+
+const player = new sprite ({
+    x: 0,
+    y: 0,
+})
+player.draw()
+const enemy = new sprite({
+    x: 400,
+    y: 100,
+})
+enemy.draw()
+
+console.log(player)
+
+function animate(){
+    window.requestAnimationFrame(animate)
+console.log('go');
+
+}
+animate()
